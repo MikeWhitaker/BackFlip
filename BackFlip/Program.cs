8d54dda84ab0bc285ccdd328f1d856acbfa0474a
Michael Whitaker <whitaker.mj@gmail.com>
2022-03-14 19:54:22 +0100

    Sorted out the issue with repository.

---
+++ BackFlip/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackFlip
+{
+    class Program
+    {
+        [STAThreadAttribute]
+        static void Main(string[] args)
+        {
+            var bf = new BackFlip();
+
+            var firstArgument = args.FirstOrDefault();
+            if(firstArgument != null && firstArgument.ToLowerInvariant() == "-f")
+            {
+                bf.ReplaceClipboardContensWithFoundFilename();
+            }
+            else
+            {
+                bf.Flip();
+            }
+        }
+    }
+}
---
+++ BackFlip/logic/BackFlip.cs
@@ -0,0 +1,64 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace BackFlip
+{
+    public class BackFlip
+    {
+        public BackFlip()
+        {
+           
+        }
+        public BackFlip(string clipboardText)
+        {
+            Clipboard.SetText(clipboardText);
+        }
+
+        public bool Flip()
+        {
+            var clipboardText = Clipboard.GetText();
+
+            if (!clipboardText.Contains("\\"))
+                return false;
+
+            clipboardText = clipboardText.Replace('\\', '/');
+            Clipboard.SetText(clipboardText); // This means that the test would have to access the clipboard.
+
+            return true;
+        }
+
+        public string GetClipboardText() { // Mainly for the tests.
+            return Clipboard.GetText();
+        }
+
+        public bool ReplaceClipboardContensWithFoundFilename()
+        {
+            var clipboardText = Clipboard.GetText();
+
+            var windowsPattern = @"^.*\\(.+\\)*(.+)\.(.+)$";
+            var linuxPattern = @"^.*\/(.+\/)*(.+)\.(.+)$";
+            var matchWindows = Regex.Match(clipboardText, windowsPattern);
+            var matchLinux = Regex.Match(clipboardText, linuxPattern);
+
+            if (!matchWindows.Success && !matchLinux.Success) //should try to match based on linux file path
+                return false;
+
+            Match match;
+
+            if (matchWindows.Success)
+            {
+                match = matchWindows;
+            } else
+            {
+                match = matchLinux;
+            }
+
+            var filename = match.Groups[2].Value;
+            var filenextention = match.Groups[3].Value;
+            var filenameWithExtention = string.Format("{0}.{1}", filename, filenextention);
+            Clipboard.SetText(filenameWithExtention);
+
+            return true;
+        }
+    }
+}
